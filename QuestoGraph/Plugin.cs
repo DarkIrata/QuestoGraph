4c4d93161ecf0520137edf09fdec912bb6f871ee
DarkIrata <git@ipmix.de>
2026-01-03 16:00:56 +0100

    Fixed loading plugin

--- QuestoGraph/Plugin.cs
+++ QuestoGraph/Plugin.cs
@@ -14,28 +14,28 @@ namespace QuestoGraph
         public const string Name = "Quest'o'Graph";
 
         [PluginService]
-        internal static IDalamudPluginInterface Interface { get; } = null!;
+        internal static IDalamudPluginInterface Interface { get; private set; } = null!;
 
         [PluginService]
-        internal static IClientState ClientState { get; } = null!;
+        internal static IClientState ClientState { get; private set; } = null!;
 
         [PluginService]
-        internal static ICommandManager CommandManager { get; } = null!;
+        internal static ICommandManager CommandManager { get; private set; } = null!;
 
         [PluginService]
-        internal static IDataManager DataManager { get; } = null!;
+        internal static IDataManager DataManager { get; private set; } = null!;
 
         [PluginService]
-        internal static ITextureProvider TextureProvider { get; } = null!;
+        internal static ITextureProvider TextureProvider { get; private set; } = null!;
 
         [PluginService]
-        internal static IGameGui GameGui { get; } = null!;
+        internal static IGameGui GameGui { get; private set; } = null!;
 
         [PluginService]
-        internal static IPluginLog Log { get; } = null!;
+        internal static IPluginLog Log { get; private set; } = null!;
 
         [PluginService]
-        internal static IChatGui ChatGui { get; } = null!;
+        internal static IChatGui ChatGui { get; private set; } = null!;
 
         private Config Config { get; }
 
